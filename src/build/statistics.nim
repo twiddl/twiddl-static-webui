ef8a9e46a0e731f51719ec2aa019bb85f80ddc94
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-08-07 14:25:40 +0200

    WIP add total successful / total failed builds

--- src/build/statistics.nim
+++ src/build/statistics.nim
@@ -1,4 +1,4 @@
-import htmlgen, ropes
+import htmlgen, ropes, sequtils
 
 import twiddl
 
@@ -11,6 +11,11 @@ proc buildStatistics*(tw:TwiddlEnv): string =
 
   result.add(h1("Statistics"))
   result.addf("Total number of builds: $1 \n", [tw.builds.len.rope])
+  result.addf("Total number of successful builds: $1 \n",
+              [tw.builds.filterIt(it.status == bsFinishedSuccessful).len.rope])
+  result.addf("Total number of failed builds: $1 \n",
+              [tw.builds.filterIt(it.status == bsFinishedFailed).len.rope])
+
   result.add(h2("Success rate"))
 
   result.add(h2("Past 30 days"))
